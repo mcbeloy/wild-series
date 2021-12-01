4956769fe0afff4ecc70bd05a84a8c1823c870a8
MaiwennBeloy <mcbeloy16@gmail.com>
2021-12-01 12:12:05 +0100

    Routes show creation

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/program", name="program_")
+*/
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -17,4 +21,13 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+
+      /**
+      * @Route("/{id}", requirements={"id"="\d+"}, name="program_show", methods={"GET"})
+      */
+
+    public function show(int $id): Response
+{
+    return $this->render('program/show.html.twig', ['program_id' => $id]);
+}
 }
